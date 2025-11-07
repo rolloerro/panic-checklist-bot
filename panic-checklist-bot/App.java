288cf7f4aedaad2cae099799fe6e3e8f19028418
MSL <pharmdoktor1972@icloud.com>
2025-11-07 21:19:55 +0300

    Update bot token in App.java

--- panic-checklist-bot/App.java
+++ panic-checklist-bot/App.java
@@ -5,5 +5,5 @@ public String getBotUsername() {
 
 @Override
 public String getBotToken() {
-    return "8498713644:AAGZCPRIfb38npKEdpEZwuZ7sBTkYKUXRRk";
+    return "+++++++++++++++++++++++++++++++++++++++++++++++++++";
 }
